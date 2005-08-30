5eb68a32443a141ceb0c3f21f442b1e9f79c010d
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2005-08-30 14:10:39 +0000

    changed to act (untested)
    git-svn-id: http://svn.techno-weenie.net/projects/paranoid-ar@69 567b1171-46fb-0310-a4c9-b4bef9110e78

--- lib/paranoid-ar.rb
+++ lib/paranoid-ar.rb
@@ -1,40 +1,69 @@
 ActiveRecord::Base.class_eval do
-  alias_method :destroy!, :destroy
-  
-  class << self
-    alias_method :find_with_deleted, :find
-    alias_method :count_with_deleted, :count
-    
-    def find(*args)
-      if [:all, :first].include?(args.first)
-        constrain = "#{table_name}.deleted_at IS NULL"
-        constrains = (scope_constrains.nil? or scope_constrains[:conditions].nil? or scope_constrains[:conditions] == constrain) ?
-          constrain :
-          "#{scope_constrains[:conditions]} AND #{constrain}"
-        constrain(:conditions => constrains) { return find_with_deleted(*args) }
+  include ActiveRecord::Acts::Paranoid
+end
+
+module ActiveRecord
+  module Acts #:nodoc:
+    module Paranoid
+      def self.append_features(base) # :nodoc:
+        super
+        base.extend ClassMethods
       end
-      find_with_deleted(*args)
+
+      # Specify this act 
+      module ClassMethods
+        def acts_as_paranoid
+          class_eval do
+            alias_method :destroy!, :destroy
+            class << self
+              alias_method :find_with_deleted, :find
+              alias_method :count_with_deleted, :count
+            end
+            include ParanoidMethods
+          end
+        end
+      end
     end
     
-    def count(conditions = nil, joins = nil)
-      constrain(:conditions => "#{table_name}.deleted_at IS NULL") { count_with_deleted(conditions, joins) }
+    module ParanoidMethods #:nodoc:
+      def self.append_features(base) # :nodoc:
+        super
+        base.extend ClassMethods
+      end
+      
+      module ClassMethods
+        def find(*args)
+          if [:all, :first].include?(args.first)
+            constrain = "#{table_name}.deleted_at IS NULL"
+            constrains = (scope_constrains.nil? or scope_constrains[:conditions].nil? or scope_constrains[:conditions] == constrain) ?
+              constrain :
+              "#{scope_constrains[:conditions]} AND #{constrain}"
+            constrain(:conditions => constrains) { return find_with_deleted(*args) }
+          end
+          find_with_deleted(*args)
+        end
+    
+        def count(conditions = nil, joins = nil)
+          constrain(:conditions => "#{table_name}.deleted_at IS NULL") { count_with_deleted(conditions, joins) }
+        end
+      end
+      
+      def destroy
+        unless new_record?
+          sql = self.class.send(:sanitize_sql,
+            ["UPDATE #{self.class.table_name} SET deleted_at = ? WHERE id = ?", 
+              self.class.default_timezone == :utc ? Time.now.utc : Time.now, id])
+          self.connection.update(sql)
+        end
+        freeze
+      end
     end
-  end
-  
-  def destroy
-    unless new_record?
-      sql = self.class.send(:sanitize_sql,
-        ["UPDATE #{self.class.table_name} SET deleted_at = ? WHERE id = ?", 
-          self.class.default_timezone == :utc ? Time.now.utc : Time.now, id])
-      self.connection.update(sql)
-    end
-    freeze
   end
 end
 
-ActiveRecord::Associations::HasAndBelongsToManyAssociation.class_eval do
-  alias_method :find_with_deleted, :find
-  def find(*args)
-    constrain(:conditions => "#{@join_table}.deleted_at IS NULL") { find_with_deleted(*args) }
-  end
-end
+#ActiveRecord::Associations::HasAndBelongsToManyAssociation.class_eval do
+#  alias_method :find_with_deleted, :find
+#  def find(*args)
+#    constrain(:conditions => "#{@join_table}.deleted_at IS NULL") { find_with_deleted(*args) }
+#  end
+#end
