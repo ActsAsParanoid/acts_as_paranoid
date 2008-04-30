af6ce1ef95e867c0fe66ac03ee88a83bc097073d
Matt Grayson <mgrayson@auto3.lib.utmem.edu>
2008-05-01 00:13:56 +0800

    Added recover_with_associations! (and tests) to recover record + any specified paranoid associations
    Signed-off-by: rick <technoweenie@gmail.com>

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,7 +7,7 @@ require 'active_record/fixtures'
 
 config = YAML::load(IO.read(File.dirname(__FILE__) + '/database.yml'))
 ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + "/debug.log")
-ActiveRecord::Base.establish_connection(config[ENV['DB'] || 'sqlite'])
+ActiveRecord::Base.establish_connection(config[ENV['DB'] || 'sqlite3'])
 
 load(File.dirname(__FILE__) + "/schema.rb")
 
