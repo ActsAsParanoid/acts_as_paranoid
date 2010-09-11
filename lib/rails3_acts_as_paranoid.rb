1d628705eca6ff8f296d484bf8092e1a1e43847b
Gonçalo Silva <goncalossilva@gmail.com>
2010-09-11 01:41:53 +0100

    whitespace removal

--- lib/rails3_acts_as_paranoid.rb
+++ lib/rails3_acts_as_paranoid.rb
@@ -1,9 +1,9 @@
 require 'active_record'
 
-module ActsAsParanoid  
+module ActsAsParanoid
   def acts_as_paranoid(options = {})
     raise ArgumentError, "Hash expected, got #{options.class.name}" if not options.is_a?(Hash) and not options.empty?
-    
+
     configuration = { :column => "deleted_at", :column_type => "time" }
     configuration.update(options) unless options.nil?
 
@@ -13,7 +13,7 @@ module ActsAsParanoid
       else
         raise ArgumentError, "'time' or 'boolean' expected for :column_type option, got #{configuration[:column_type]}"
     end
-    
+
     class_eval <<-EOV
       default_scope where("#{self.table_name}.#{configuration[:column]} IS ?", nil)
 
@@ -31,7 +31,7 @@ module ActsAsParanoid
         def delete_all!(conditions = nil)
           self.unscoped.delete_all!(conditions)
         end
-        
+
         def delete_all(conditions = nil)
           update_all ["#{configuration[:column]} = ?", #{type}], conditions
         end
@@ -39,14 +39,14 @@ module ActsAsParanoid
 
       def destroy!
         before_destroy() if respond_to?(:before_destroy)
-        
+
         #{self.name}.delete_all!(:id => self)
-        
+
         after_destroy() if respond_to?(:after_destroy)
       end
-      
+
       def destroy
-        run_callbacks :destroy do          
+        run_callbacks :destroy do
           if self.#{configuration[:column]} == nil
             #{self.name}.delete_all(:id => self.id)
           else
