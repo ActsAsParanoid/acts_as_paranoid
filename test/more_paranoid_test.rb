6bd95d56e5ba049f108b1ad916395aa05c2142d7
Craig Walker <github@softcraft.ca>
2011-12-06 10:51:52 -0700

    Basic instance destroy test

---
+++ test/more_paranoid_test.rb
@@ -0,0 +1,27 @@
+require "#{File.dirname(__FILE__)}/test_helper"
+
+class MoreParanoidTest < ParanoidBaseTest
+  test "instance destroy is paranoid" do
+    model = ParanoidBelongsDependant.create
+    model.destroy
+    
+    assert_paranoid_deletion(model)
+  end
+
+  def find_row(model)
+    sql = "select deleted_at from #{model.class.table_name} where id = #{model.id}"
+    # puts sql here if you want to debug
+    model.class.connection.select_one(sql)
+  end
+  
+  def assert_paranoid_deletion(model)
+    row = find_row(model)
+    assert_not_nil row, "#{model.class} entirely deleted"
+    assert_not_nil row["deleted_at"], "Deleted at not set"
+  end
+  
+  def assert_non_paranoid_deletion(model)
+    row = find_row(model)
+    assert_nil row, "#{model.class} still exists"
+  end
+end
