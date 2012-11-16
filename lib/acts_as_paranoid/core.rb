7887adf13c37d7da79dfca435c17e2841a04c73b
Tiago Cardoso <tiago@restorm.com>
2012-11-16 16:17:00 +0100

    added support for composite primary keys

--- lib/acts_as_paranoid/core.rb
+++ lib/acts_as_paranoid/core.rb
@@ -89,7 +89,7 @@ module ActsAsParanoid
       with_transaction_returning_status do
         run_callbacks :destroy do
           destroy_dependent_associations!
-          self.class.delete_all!(self.class.primary_key.to_sym => self.id)
+          self.class.delete_all!(Hash[[Array(self.class.primary_key), Array(self.id)].transpose])
           self.paranoid_value = self.class.delete_now_value
           freeze
         end
@@ -100,7 +100,7 @@ module ActsAsParanoid
       if !deleted?
         with_transaction_returning_status do
           run_callbacks :destroy do
-            self.class.delete_all(self.class.primary_key.to_sym => self.id)
+            self.class.delete_all(Hash[[Array(self.class.primary_key), Array(self.id)].transpose])
             self.paranoid_value = self.class.delete_now_value
             self
           end
