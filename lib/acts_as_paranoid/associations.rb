5bf262e4b66d09117451fd2f32ed291110f68a18
Yuki Nishijima <mail@yukinishijima.net>
2013-03-19 19:40:30 +0900

    belongs_to_with_deleted should pass scope to belongs_to_without_deleted

--- lib/acts_as_paranoid/associations.rb
+++ lib/acts_as_paranoid/associations.rb
@@ -8,9 +8,9 @@ module ActsAsParanoid
     end
 
     module ClassMethods
-      def belongs_to_with_deleted(target, options = {})
-        with_deleted = options.delete(:with_deleted)
-        result = belongs_to_without_deleted(target, options)
+      def belongs_to_with_deleted(target, scope = nil, options = {})
+        with_deleted = (scope.is_a?(Hash) ? scope : options).delete(:with_deleted)
+        result = belongs_to_without_deleted(target, scope, options)
 
         if with_deleted
           result.options[:with_deleted] = with_deleted
