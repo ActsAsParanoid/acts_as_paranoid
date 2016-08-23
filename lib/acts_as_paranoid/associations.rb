a28beb7131691d6755c7207f1c087178fd42be19
trcarden <trcarden@gmail.com>
2016-08-23 16:18:32 -0700

    Replaces alias_method_chain with Module.prepend
    * Rails deprecated alias_method_chain in Rails 5 and recommends using Module.prepend instead.
    * Commit refactors implementation to use prepend instead.
    * Commit replaces class_eval with define_method in associations.rb, See: http://tenderlovemaking.com/2013/03/03/dynamic_method_definitions.html
    * Commit requires ruby 2.0+ to support Module.prepend, removes support for ruby 1.9 in travis
    * Commit bumps version to 0.6.0
    * Commit updates travis to remove tests for Rails 5 and < Ruby 2.2 as ActiveSupport 5.0.0.1 no longer supports < Ruby 2.2

--- lib/acts_as_paranoid/associations.rb
+++ lib/acts_as_paranoid/associations.rb
@@ -1,16 +1,15 @@
 module ActsAsParanoid
   module Associations
     def self.included(base)
-      base.extend ClassMethods
       class << base
-        alias_method_chain :belongs_to, :deleted
+        prepend(ClassMethods)
       end
     end
 
     module ClassMethods
-      def belongs_to_with_deleted(target, scope = nil, options = {})
+      def belongs_to(target, scope = nil, options = {})
         with_deleted = (scope.is_a?(Hash) ? scope : options).delete(:with_deleted)
-        result = belongs_to_without_deleted(target, scope, options)
+        result = super(target, scope, options)
 
         if with_deleted
           if result.is_a? Hash
@@ -19,16 +18,15 @@ module ActsAsParanoid
             result.options[:with_deleted] = with_deleted
           end
 
-          unless method_defined? "#{target}_with_unscoped"
-            class_eval <<-RUBY, __FILE__, __LINE__
-              def #{target}_with_unscoped(*args)
-                association = association(:#{target})
-                return nil if association.options[:polymorphic] && association.klass.nil?
-                return #{target}_without_unscoped(*args) unless association.klass.paranoid?
-                association.klass.with_deleted.scoping { #{target}_without_unscoped(*args) }
-              end
-              alias_method_chain :#{target}, :unscoped
-            RUBY
+          # Grab unbound instance method from super above
+          original_method = instance_method(target.to_sym)
+
+          # Define new accessor that honors the paranoid logic
+          define_method(target) do |*args|
+            association = association(target.to_sym)
+            return nil if association.options[:polymorphic] && association.klass.nil?
+            return original_method.bind(self).call(*args) unless association.klass.paranoid?
+            association.klass.with_deleted.scoping { original_method.bind(self).call(*args) }
           end
         end
 
