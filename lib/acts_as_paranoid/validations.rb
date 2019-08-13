add128a7cf925620415ff3d6a279d36eb021f17f
Daniel Rice <daniel.rice@ldstudios.co>
2019-08-13 11:46:43 -0400

    Revert "Map Rails 6 to "V5" validator class"
    This reverts commit bd13b83de5b166831606ddbcdfdd80c4243f8ca8.

--- lib/acts_as_paranoid/validations.rb
+++ lib/acts_as_paranoid/validations.rb
@@ -79,12 +79,7 @@ module ActsAsParanoid
 
     module ClassMethods
       def validates_uniqueness_of_without_deleted(*attr_names)
-        case ActiveRecord::VERSION::MAJOR
-        when 'V4', 'V5'
-          validates_with UniquenessWithoutDeletedValidator[ActiveRecord::VERSION::MAJOR], _merge_attributes(attr_names)
-        when '6'
-          validates_with UniquenessWithoutDeletedValidator['V5'], _merge_attributes(attr_names)
-        end
+        validates_with UniquenessWithoutDeletedValidator[ActiveRecord::VERSION::MAJOR], _merge_attributes(attr_names)
       end
     end
   end
