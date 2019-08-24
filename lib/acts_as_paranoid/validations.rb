af6dc5f1c4bd02b4514e96f2403e3c8ce94dd5fc
Daniel Rice <daniel.rice@ldstudios.co>
2019-08-24 11:40:56 -0400

    Normalize on a single Validator class

--- lib/acts_as_paranoid/validations.rb
+++ lib/acts_as_paranoid/validations.rb
@@ -6,85 +6,31 @@ module ActsAsParanoid
       base.extend ClassMethods
     end
 
-    class UniquenessWithoutDeletedValidator
-      def self.[](version)
-        name = "V#{version.to_s.tr('.', '_')}"
-        unless constants.include? name.to_sym
-          raise "Unknown validator version #{name.inspect}; expected one of #{constants.sort.join(', ')}"
-        end
-        const_get name
-      end
+    class UniquenessWithoutDeletedValidator < ActiveRecord::Validations::UniquenessValidator
+      
+      def validate_each(record, attribute, value)
+        finder_class = find_finder_class_for(record)
+        table = finder_class.arel_table
 
-      class V5 < ActiveRecord::Validations::UniquenessValidator
-        def validate_each(record, attribute, value)
-          finder_class = find_finder_class_for(record)
-          table = finder_class.arel_table
+        relation = build_relation(finder_class, attribute, value)
+        [Array(finder_class.primary_key), Array(record.send(:id))].transpose.each do |pk_key, pk_value|
+          relation = relation.where(table[pk_key.to_sym].not_eq(pk_value))
+        end if record.persisted?
 
-          relation = build_relation(finder_class, attribute, value)
-          [Array(finder_class.primary_key), Array(record.send(:id))].transpose.each do |pk_key, pk_value|
-            relation = relation.where(table[pk_key.to_sym].not_eq(pk_value))
-          end if record.persisted?
-
-          Array.wrap(options[:scope]).each do |scope_item|
-            relation = relation.where(table[scope_item].eq(record.public_send(scope_item)))
-          end
-
-          if relation.where(finder_class.paranoid_default_scope).exists?(relation)
-            record.errors.add(attribute, :taken, options.except(:case_sensitive, :scope).merge(:value => value))
-          end
+        Array.wrap(options[:scope]).each do |scope_item|
+          relation = relation.where(table[scope_item].eq(record.public_send(scope_item)))
         end
 
-        protected
-
-        def build_relation(klass, attribute, value)
-          if ActiveRecord::VERSION::MINOR == 0 && ActiveRecord::VERSION::MAJOR == 5
-            return super(klass, klass.arel_table, attribute, value)
-          else
-            super
-          end
+        if relation.where(finder_class.paranoid_default_scope).exists?(relation)
+          record.errors.add(attribute, :taken, options.except(:case_sensitive, :scope).merge(:value => value))
         end
       end
-
-      class V4 < ActiveRecord::Validations::UniquenessValidator
-        def validate_each(record, attribute, value)
-          finder_class = find_finder_class_for(record)
-          table = finder_class.arel_table
-
-          # TODO: Use record.class.column_types[attribute.to_s].coder ?
-          coder = record.class.column_types[attribute.to_s]
-
-          if value && coder
-            value = if coder.respond_to? :type_cast_for_database
-                      coder.type_cast_for_database value
-                    else
-                      coder.type_cast_for_write value
-                    end
-          end
-
-          relation = build_relation(finder_class, table, attribute, value)
-          [Array(finder_class.primary_key), Array(record.send(:id))].transpose.each do |pk_key, pk_value|
-            relation = relation.and(table[pk_key.to_sym].not_eq(pk_value))
-          end if record.persisted?
-
-          Array.wrap(options[:scope]).each do |scope_item|
-            scope_value = record.send(scope_item)
-            relation = relation.and(table[scope_item].eq(scope_value))
-          end
-
-          # Re-add ActsAsParanoid default scope conditions manually.
-          if finder_class.unscoped.where(finder_class.paranoid_default_scope).where(relation).exists?
-            record.errors.add(attribute, :taken, options.except(:case_sensitive, :scope).merge(:value => value))
-          end
-        end
-      end
-
-      class V6 < V5
-      end
+    
     end
 
     module ClassMethods
       def validates_uniqueness_of_without_deleted(*attr_names)
-        validates_with UniquenessWithoutDeletedValidator[ActiveRecord::VERSION::MAJOR], _merge_attributes(attr_names)
+        validates_with UniquenessWithoutDeletedValidator, _merge_attributes(attr_names)
       end
     end
   end
