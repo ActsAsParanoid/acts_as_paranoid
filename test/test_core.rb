f1eec93a8de1a8e2ea0823bae06f9aaf83b2b862
Matijs van Zuijlen <matijs@matijs.net>
2022-04-04 23:16:27 +0200

    Pull test setup into ParanoidTest
    Each test class now has its own migration and classes.

--- test/test_core.rb
+++ test/test_core.rb
@@ -2,7 +2,307 @@
 
 require "test_helper"
 
-class ParanoidTest < ParanoidBaseTest
+class ParanoidTest < ActiveSupport::TestCase
+  class ParanoidTime < ActiveRecord::Base
+    acts_as_paranoid
+
+    validates_uniqueness_of :name
+
+    has_many :paranoid_has_many_dependants, dependent: :destroy
+    has_many :paranoid_booleans, dependent: :destroy
+    has_many :not_paranoids, dependent: :delete_all
+    has_many :paranoid_sections, dependent: :destroy
+
+    has_one :has_one_not_paranoid, dependent: :destroy
+
+    belongs_to :not_paranoid, dependent: :destroy
+
+    attr_accessor :destroyable
+
+    before_destroy :ensure_destroyable
+
+    protected
+
+    def ensure_destroyable
+      return if destroyable.nil?
+
+      throw(:abort) unless destroyable
+    end
+  end
+
+  class ParanoidBoolean < ActiveRecord::Base
+    acts_as_paranoid column_type: "boolean", column: "is_deleted"
+    validates_as_paranoid
+    validates_uniqueness_of_without_deleted :name
+
+    belongs_to :paranoid_time
+    has_one :paranoid_has_one_dependant, dependent: :destroy
+    has_many :paranoid_with_counter_cache, dependent: :destroy
+    has_many :paranoid_with_custom_counter_cache, dependent: :destroy
+    has_many :paranoid_with_touch_and_counter_cache, dependent: :destroy
+    has_many :paranoid_with_touch, dependent: :destroy
+  end
+
+  class ParanoidString < ActiveRecord::Base
+    acts_as_paranoid column_type: "string", column: "deleted", deleted_value: "dead"
+  end
+
+  class NotParanoid < ActiveRecord::Base
+    has_many :paranoid_times
+  end
+
+  class ParanoidNoDoubleTapDestroysFully < ActiveRecord::Base
+    acts_as_paranoid double_tap_destroys_fully: false
+  end
+
+  class HasOneNotParanoid < ActiveRecord::Base
+    belongs_to :paranoid_time, with_deleted: true
+  end
+
+  class ParanoidWithCounterCache < ActiveRecord::Base
+    acts_as_paranoid
+    belongs_to :paranoid_boolean, counter_cache: true
+  end
+
+  class ParanoidWithCustomCounterCache < ActiveRecord::Base
+    self.table_name = "paranoid_with_counter_caches"
+
+    acts_as_paranoid
+    belongs_to :paranoid_boolean, counter_cache: :custom_counter_cache
+  end
+
+  class ParanoidWithCounterCacheOnOptionalBelognsTo < ActiveRecord::Base
+    self.table_name = "paranoid_with_counter_caches"
+
+    acts_as_paranoid
+    belongs_to :paranoid_boolean, counter_cache: true, optional: true
+  end
+
+  class ParanoidWithTouch < ActiveRecord::Base
+    self.table_name = "paranoid_with_counter_caches"
+    acts_as_paranoid
+    belongs_to :paranoid_boolean, touch: true
+  end
+
+  class ParanoidWithTouchAndCounterCache < ActiveRecord::Base
+    self.table_name = "paranoid_with_counter_caches"
+    acts_as_paranoid
+    belongs_to :paranoid_boolean, touch: true, counter_cache: true
+  end
+
+  class ParanoidHasManyDependant < ActiveRecord::Base
+    acts_as_paranoid
+
+    belongs_to :paranoid_belongs_dependant, dependent: :destroy
+  end
+
+  class ParanoidBelongsDependant < ActiveRecord::Base
+    acts_as_paranoid
+
+    has_many :paranoid_has_many_dependants
+  end
+
+  class ParanoidHasOneDependant < ActiveRecord::Base
+    acts_as_paranoid
+
+    belongs_to :paranoid_boolean
+  end
+
+  class ParanoidWithCallback < ActiveRecord::Base
+    acts_as_paranoid
+
+    attr_accessor :called_before_destroy, :called_after_destroy,
+                  :called_after_commit_on_destroy, :called_before_recover,
+                  :called_after_recover
+
+    before_destroy :call_me_before_destroy
+    after_destroy :call_me_after_destroy
+
+    after_commit :call_me_after_commit_on_destroy, on: :destroy
+
+    before_recover :call_me_before_recover
+    after_recover :call_me_after_recover
+
+    def initialize(*attrs)
+      @called_before_destroy = false
+      @called_after_destroy = false
+      @called_after_commit_on_destroy = false
+      super(*attrs)
+    end
+
+    def call_me_before_destroy
+      @called_before_destroy = true
+    end
+
+    def call_me_after_destroy
+      @called_after_destroy = true
+    end
+
+    def call_me_after_commit_on_destroy
+      @called_after_commit_on_destroy = true
+    end
+
+    def call_me_before_recover
+      @called_before_recover = true
+    end
+
+    def call_me_after_recover
+      @called_after_recover = true
+    end
+  end
+
+  class ParanoidPolygon < ActiveRecord::Base
+    acts_as_paranoid
+    default_scope { where("sides = ?", 3) }
+  end
+
+  class ParanoidAndroid < ActiveRecord::Base
+    acts_as_paranoid
+  end
+
+  class ParanoidSection < ActiveRecord::Base
+    acts_as_paranoid
+    belongs_to :paranoid_time
+    belongs_to :paranoid_thing, polymorphic: true, dependent: :destroy
+  end
+
+  class ParanoidBooleanNotNullable < ActiveRecord::Base
+    acts_as_paranoid column: "deleted", column_type: "boolean", allow_nulls: false
+  end
+
+  class ParanoidWithExplicitTableNameAfterMacro < ActiveRecord::Base
+    acts_as_paranoid
+    self.table_name = "explicit_table"
+  end
+
+  # rubocop:disable Metrics/AbcSize
+  def setup_db
+    ActiveRecord::Schema.define(version: 1) do # rubocop:disable Metrics/BlockLength
+      create_table :paranoid_times do |t|
+        t.string    :name
+        t.datetime  :deleted_at
+        t.integer   :paranoid_belongs_dependant_id
+        t.integer   :not_paranoid_id
+
+        timestamps t
+      end
+
+      create_table :paranoid_booleans do |t|
+        t.string    :name
+        t.boolean   :is_deleted
+        t.integer   :paranoid_time_id
+        t.integer   :paranoid_with_counter_caches_count
+        t.integer   :paranoid_with_touch_and_counter_caches_count
+        t.integer   :custom_counter_cache
+        timestamps t
+      end
+
+      create_table :paranoid_strings do |t|
+        t.string    :name
+        t.string    :deleted
+      end
+
+      create_table :not_paranoids do |t|
+        t.string    :name
+        t.integer   :paranoid_time_id
+
+        timestamps t
+      end
+
+      create_table :has_one_not_paranoids do |t|
+        t.string    :name
+        t.integer   :paranoid_time_id
+
+        timestamps t
+      end
+
+      create_table :paranoid_has_many_dependants do |t|
+        t.string    :name
+        t.datetime  :deleted_at
+        t.integer   :paranoid_time_id
+        t.string    :paranoid_time_polymorphic_with_deleted_type
+        t.integer   :paranoid_belongs_dependant_id
+
+        timestamps t
+      end
+
+      create_table :paranoid_belongs_dependants do |t|
+        t.string    :name
+        t.datetime  :deleted_at
+
+        timestamps t
+      end
+
+      create_table :paranoid_has_one_dependants do |t|
+        t.string    :name
+        t.datetime  :deleted_at
+        t.integer   :paranoid_boolean_id
+
+        timestamps t
+      end
+
+      create_table :paranoid_with_callbacks do |t|
+        t.string    :name
+        t.datetime  :deleted_at
+
+        timestamps t
+      end
+
+      create_table :paranoid_polygons do |t|
+        t.integer :sides
+        t.datetime :deleted_at
+
+        timestamps t
+      end
+
+      create_table :paranoid_androids do |t|
+        t.datetime :deleted_at
+      end
+
+      create_table :paranoid_sections do |t|
+        t.integer   :paranoid_time_id
+        t.integer   :paranoid_thing_id
+        t.string    :paranoid_thing_type
+        t.datetime :deleted_at
+      end
+
+      create_table :paranoid_boolean_not_nullables do |t|
+        t.string :name
+        t.boolean :deleted, :boolean, null: false, default: false
+      end
+
+      create_table :paranoid_no_double_tap_destroys_fullies do |t|
+        t.datetime :deleted_at
+      end
+
+      create_table :paranoid_with_counter_caches do |t|
+        t.string    :name
+        t.datetime  :deleted_at
+        t.integer   :paranoid_boolean_id
+
+        timestamps t
+      end
+    end
+  end
+  # rubocop:enable Metrics/AbcSize
+
+  def setup
+    setup_db
+
+    ["paranoid", "really paranoid", "extremely paranoid"].each do |name|
+      ParanoidTime.create! name: name
+      ParanoidBoolean.create! name: name
+    end
+
+    ParanoidString.create! name: "strings can be paranoid"
+    NotParanoid.create! name: "no paranoid goals"
+    ParanoidWithCallback.create! name: "paranoid with callbacks"
+  end
+
+  def teardown
+    teardown_db
+  end
+
   def test_paranoid?
     refute_predicate NotParanoid, :paranoid?
     assert_raise(NoMethodError) { NotParanoid.delete_all! }
