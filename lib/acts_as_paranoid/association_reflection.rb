88cc53b77b7a7f02e272249dfa2785fb4439c206
Matijs van Zuijlen <matijs@matijs.net>
2022-04-18 22:25:11 +0200

    Update code comments

--- lib/acts_as_paranoid/association_reflection.rb
+++ lib/acts_as_paranoid/association_reflection.rb
@@ -2,6 +2,17 @@
 
 module ActsAsParanoid
   # Override for ActiveRecord::Reflection::AssociationReflection
+  #
+  # This makes automatic finding of inverse associations work where the
+  # inverse is a belongs_to association with the :with_deleted option set.
+  #
+  # Specifying :with_deleted for the belongs_to association would stop the
+  # inverse from being calculated because it sets scope where there was none,
+  # and normally an association having a scope means ActiveRecord will not
+  # automatically find the inverse association.
+  #
+  # This override adds an exception to that rule only for the case where the
+  # scope was added just to support the :with_deleted option.
   module AssociationReflection
     if ActiveRecord::VERSION::MAJOR < 7
       def can_find_inverse_of_automatically?(reflection)
