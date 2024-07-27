a7409c2f79a7dc5149ec7b0f293500843464cfb4
Matijs van Zuijlen <matijs@matijs.net>
2024-07-27 11:27:28 +0200

    Document ActsAsParanoid::Associations

--- lib/acts_as_paranoid/associations.rb
+++ lib/acts_as_paranoid/associations.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module ActsAsParanoid
+  # This module is included in ActiveRecord::Base to provide paranoid associations.
   module Associations
     def self.included(base)
       base.extend ClassMethods
